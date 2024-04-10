543569d8b0fa6e4212fd1d5f01fae92f136170ae
Hemant D <50235905+h3mantD@users.noreply.github.com>
2024-04-10 18:08:59 +0100

    Update Welcome.vue to include a new section for EmployeeOps introduction

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -49,7 +49,7 @@ defineProps({
             </template>
         </div>
 
-        <div class="max-w-7xl mx-auto p-6 lg:p-8">
+        <!-- <div class="max-w-7xl mx-auto p-6 lg:p-8">
             <div class="flex justify-center">
                 <svg
                     viewBox="0 0 62 65"
@@ -331,7 +331,47 @@ defineProps({
                     Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
                 </div>
             </div>
-        </div>
+        </div> -->
+
+        <section class="bg-gray-900 text-white">
+            <div
+                class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
+            >
+                <div class="mx-auto max-w-3xl text-center">
+                    <h1
+                        class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+                    >
+                        Streamline Workforce Dynamics,
+
+                        <span class="sm:block"> Maximize Impact. </span>
+                    </h1>
+
+                    <p class="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+                        EmployeeOps empowers organizations by streamlining
+                        workforce operations, fostering seamless collaboration
+                        and efficient resource allocation. Harness the full
+                        potential of your team to maximize productivity and
+                        drive impactful results.
+                    </p>
+
+                    <div class="mt-8 flex flex-wrap justify-center gap-4">
+                        <a
+                            class="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+                            href="#"
+                        >
+                            Get Started
+                        </a>
+
+                        <a
+                            class="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
+                            href="#"
+                        >
+                            Learn More
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
